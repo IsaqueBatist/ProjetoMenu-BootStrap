7aa65ba00e9c2cf8f95ac43b567ef4bdc05abc2a
Issaque Batista Barbosa <isaquebatista400@gmail.com>
2024-04-16 12:58:31 -0300

    closes #10 closes #11 - Mascara e calc

--- assets/js/calc.js
+++ assets/js/calc.js
@@ -1,14 +1,9 @@
 function calc() {
   var formater = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' })
   var total = 0
-  var resp = document.getElementById('resp')
+  var resp = document.querySelector('#resp')
   var name = document.getElementById('iname')
-  var phone = document.getElementById('iphone')
-  var mail = document.getElementById('imail')
   resp.innerHTML = ''
-  if (name.value.length == 0 || phone.value.length == 0 || mail.value.length == 0){
-    alert('Por favor,digite seus dados')
-  } else {
   resp.innerHTML += `<p>Caro <strong>${name.value}</strong></p><p>Segue os dados do seu pedido</p><p>O seu pedido é:</p>`
   var dishes = document.getElementsByName('quantity')
   for (var input of dishes) {
@@ -22,4 +17,3 @@ function calc() {
   }
   resp.innerHTML += `<p><strong>Preço final: ${formater.format(total)}</strong></p>`
   }
-}
